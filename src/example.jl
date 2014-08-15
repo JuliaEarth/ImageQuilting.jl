70c82b0214d32cd1d23e56ec7e79f3f59356f0ac
Júlio Hoffimann <julio.hoffimann@gmail.com>
2014-08-15 11:34:37 -0300

    Use Pkg.dir() to find where sample images are located

--- src/example.jl
+++ src/example.jl
@@ -24,9 +24,10 @@ include("imquilt.jl")
 
 function example()
     srand(2014) # make sure results are reproducible
-    imquilt(imread(joinpath("images","radishes.jpg")), 64, 5 , show=true)
-    imquilt(imread(joinpath("images","weave.jpg"))   , 80, 5 , show=true)
-    imquilt(imread(joinpath("images","brick.jpg"))   , 80, 5 , show=true)
-    imquilt(imread(joinpath("images","apples.gif"))  , 31, 15, show=true)
-    imquilt(imread(joinpath("images","btile.tif"))   , 16, 15, show=true)
+    imagesdir = joinpath(Pkg.dir(),"ImageQuilting","src","images")
+    imquilt(imread(joinpath(imagesdir,"radishes.jpg")), 64, 5 , show=true)
+    imquilt(imread(joinpath(imagesdir,"weave.jpg"))   , 80, 5 , show=true)
+    imquilt(imread(joinpath(imagesdir,"brick.jpg"))   , 80, 5 , show=true)
+    imquilt(imread(joinpath(imagesdir,"apples.gif"))  , 31, 15, show=true)
+    imquilt(imread(joinpath(imagesdir,"btile.tif"))   , 16, 15, show=true)
 end
