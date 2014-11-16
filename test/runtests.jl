c7b7d51ce171e3f3bd3ba75a58d8caac38005474
Júlio Hoffimann <julio.hoffimann@gmail.com>
2014-11-16 13:44:13 -0800

    Add tests for ArgumentError exception

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,9 @@ using Base.Test
 
 # the output of a homogeneous image is also homogeneous
 @test all(imquilt(grayim(ones(100,100)), 12, 5) .== 1)
+
+# tile size must fit in image
+@test_throws ArgumentError imquilt(grayim(eye(10)),20,5)
+
+# tile size must be at least 12
+@test_throws ArgumentError imquilt(grayim(eye(20)),10,5)
