421d43b32a04a40d070e563347cefb4dff83fb46
Júlio Hoffimann <julio.hoffimann@gmail.com>
2015-06-28 21:05:01 -0700

    Reduce grid size further in test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,4 @@ using ImageQuilting
 using Base.Test
 
 # the output of a homogeneous image is also homogeneous
-@test all(iqsim(ones(50,50,50), 10, 10, 10, 50, 50, 50)[1] .== 1)
+@test all(iqsim(ones(20,20,20), 10, 10, 10, 20, 20, 20)[1] .== 1)
