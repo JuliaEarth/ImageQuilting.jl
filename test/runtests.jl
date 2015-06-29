06d2531ac3f5140d0eb34ee3cf8a829c259d6681
Júlio Hoffimann <julio.hoffimann@gmail.com>
2015-06-28 22:00:22 -0700

    Add test for categorical training image

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,11 @@ using ImageQuilting
 using Base.Test
 
 # the output of a homogeneous image is also homogeneous
-@test all(iqsim(ones(20,20,20), 10, 10, 10, 20, 20, 20)[1] .== 1)
+TI = ones(20,20,20)
+reals = iqsim(TI, 10, 10, 10, size(TI)...)
+@test all(reals[1] .== 1)
+
+# categories are obtained from training image only
+ncateg = 3; TI = rand(RandomDevice(), 0:ncateg, 20, 20, 20)
+reals = iqsim(TI, 10, 10, 10, size(TI)..., categorical=true)
+@test Set(reals[1]) ⊆ Set(TI)
