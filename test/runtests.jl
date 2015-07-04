cee05c8e1f60132ca6c3d7d443cd46686423a0fa
Júlio Hoffimann <julio.hoffimann@gmail.com>
2015-07-03 18:30:25 -0700

    Add test for soft data

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,3 +10,9 @@ reals = iqsim(TI, 10, 10, 10, size(TI)...)
 ncateg = 3; TI = rand(RandomDevice(), 0:ncateg, 20, 20, 20)
 reals = iqsim(TI, 10, 10, 10, size(TI)..., categorical=true)
 @test Set(reals[1]) ⊆ Set(TI)
+
+# trends with soft data
+TI = [zeros(10,20,1); ones(10,20,1)]
+trend = [zeros(20,10,1) ones(20,10,1)]
+reals = iqsim(TI, 10, 10, 1, size(TI)..., soft=SoftData(trend, x -> x), cutoff=1)
+@test mean(reals[1][:,1:10,:]) ≤ mean(reals[1][:,11:20,:])
