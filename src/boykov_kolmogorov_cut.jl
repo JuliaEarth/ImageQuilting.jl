d5cdd1ae83b4242c05d526ae280dd8c46397358a
Júlio Hoffimann <julio.hoffimann@gmail.com>
2016-04-09 13:45:25 -0700

    Use gradients in Boykov-Kolmogorov cut

--- src/boykov_kolmogorov_cut.jl
+++ src/boykov_kolmogorov_cut.jl
@@ -15,17 +15,51 @@
 function boykov_kolmogorov_cut(A::AbstractArray, B::AbstractArray, dir::Symbol)
   # permute dimensions so that the algorithm is
   # the same for cuts in x, y and z directions
-  E = abs(A - B)
   if dir == :x
-    E = permutedims(E, [1,2,3])
+    A = permutedims(A, [1,2,3])
+    B = permutedims(B, [1,2,3])
   elseif dir == :y
-    E = permutedims(E, [2,1,3])
+    A = permutedims(A, [2,1,3])
+    B = permutedims(B, [2,1,3])
   elseif dir == :z
-    E = permutedims(E, [3,2,1])
+    A = permutedims(A, [3,2,1])
+    B = permutedims(B, [3,2,1])
   end
+
+  E = abs(A - B)
 
   mx, my, mz = size(E)
   nvox = mx*my*mz
+
+  # compute gradients
+  ∇xₐ = similar(E); ∇yₐ = similar(E); ∇zₐ = similar(E)
+  ∇xᵦ = similar(E); ∇yᵦ = similar(E); ∇zᵦ = similar(E)
+  for i=1:mx-1, j=1:my, k=1:mz
+    ∇xₐ[i,j,k] = A[i+1,j,k] - A[i,j,k]
+    ∇xᵦ[i,j,k] = B[i+1,j,k] - B[i,j,k]
+  end
+  for i=1:mx, j=1:my-1, k=1:mz
+    ∇yₐ[i,j,k] = A[i,j+1,k] - A[i,j,k]
+    ∇yᵦ[i,j,k] = B[i,j+1,k] - B[i,j,k]
+  end
+  for i=1:mx, j=1:my, k=1:mz-1
+    ∇zₐ[i,j,k] = A[i,j,k+1] - A[i,j,k]
+    ∇zᵦ[i,j,k] = B[i,j,k+1] - B[i,j,k]
+  end
+  if mx > 1
+    ∇xₐ[mx,:,:] = ∇xₐ[mx-1,:,:]
+    ∇xᵦ[mx,:,:] = ∇xᵦ[mx-1,:,:]
+  end
+  if my > 1
+    ∇yₐ[:,my,:] = ∇yₐ[:,my-1,:]
+    ∇yᵦ[:,my,:] = ∇yᵦ[:,my-1,:]
+  end
+  if mz > 1
+    ∇zₐ[:,:,mz] = ∇zₐ[:,:,mz-1]
+    ∇zᵦ[:,:,mz] = ∇zᵦ[:,:,mz-1]
+  end
+  ∇xₐ = abs(∇xₐ); ∇yₐ = abs(∇yₐ); ∇zₐ = abs(∇zₐ)
+  ∇xᵦ = abs(∇xᵦ); ∇yᵦ = abs(∇yᵦ); ∇zᵦ = abs(∇zᵦ)
 
   # add source and sink terminals
   s = nvox + 1; t = nvox + 2
@@ -38,21 +72,21 @@ function boykov_kolmogorov_cut(A::AbstractArray, B::AbstractArray, dir::Symbol)
     d = sub2ind((mx,my,mz), i+1, j, k)
     add_edge!(G, c, d)
     add_edge!(G, d, c)
-    C[c,d] = C[d,c] = E[c] + E[d]
+    C[c,d] = C[d,c] = (E[c] + E[d]) / (∇xₐ[c] + ∇xₐ[d] + ∇xᵦ[c] + ∇xᵦ[d])
   end
   for k=1:mz, j=1:my-1, i=1:mx
     c = sub2ind((mx,my,mz), i, j, k)
     r = sub2ind((mx,my,mz), i, j+1, k)
     add_edge!(G, c, r)
     add_edge!(G, r, c)
-    C[c,r] = C[r,c] = E[c] + E[r]
+    C[c,r] = C[r,c] = (E[c] + E[r]) / (∇yₐ[c] + ∇yₐ[r] + ∇yᵦ[c] + ∇yᵦ[r])
   end
   for k=1:mz-1, j=1:my, i=1:mx
     c = sub2ind((mx,my,mz), i, j, k)
     o = sub2ind((mx,my,mz), i, j, k+1)
     add_edge!(G, c, o)
     add_edge!(G, o, c)
-    C[c,o] = C[o,c] = E[c] + E[o]
+    C[c,o] = C[o,c] = (E[c] + E[o]) / (∇zₐ[c] + ∇zₐ[o] + ∇zᵦ[c] + ∇zᵦ[o])
   end
   for k=1:mz, j=1:my
     u = sub2ind((mx,my,mz), 1, j, k)
