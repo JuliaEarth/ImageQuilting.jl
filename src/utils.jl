871c674a16fd4cf729532d68cac9087a3af8b86d
Júlio Hoffimann <julio.hoffimann@gmail.com>
2016-10-11 20:21:10 -0700

    Flip :rasterup and :rasterdown path options

--- src/utils.jl
+++ src/utils.jl
@@ -44,13 +44,13 @@ function genpath(extent::NTuple{3,Integer}, kind::Symbol, datum=Set())
   path = Int[]
 
   if kind == :rasterup
-    for k=extent[3]:-1:1, j=1:extent[2], i=1:extent[1]
+    for k=1:extent[3], j=1:extent[2], i=1:extent[1]
       push!(path, sub2ind(extent, i,j,k))
     end
   end
 
   if kind == :rasterdown
-    for k=1:extent[3], j=1:extent[2], i=1:extent[1]
+    for k=extent[3]:-1:1, j=1:extent[2], i=1:extent[1]
       push!(path, sub2ind(extent, i,j,k))
     end
   end
