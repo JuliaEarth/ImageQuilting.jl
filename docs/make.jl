0e5822054930fab7284b46d97f8ad7d71e9f198f
Júlio Hoffimann <julio.hoffimann@gmail.com>
2016-11-02 11:41:05 -0700

    Add markdown math extension to docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, ImageQuilting
 makedocs()
 
 deploydocs(
-  deps  = Deps.pip("mkdocs"),
+  deps  = Deps.pip("mkdocs","python-markdown-math"),
   repo  = "github.com/juliohm/ImageQuilting.jl.git",
   julia = "0.5"
 )
