b8216fd7ec2dbbe0d779939d4c42199bc08c7edf
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-04-05 12:27:05 -0700

    Remove print statements from benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -10,8 +10,6 @@ iqsim(TI, 10, 10, 10, size(TI)...) # warm up
 @benchgroup "2D simulation" ["iqsim"] begin
   for TIname in ["Strebelle", "StoneWall"]
     TI = training_image(TIname)
-    println(size(TI))
-    flush(STDOUT)
     @bench TIname iqsim(TI, 30, 30, 1, size(TI)..., nreal=3)
   end
 end
