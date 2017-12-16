bcc4f4472414805e348d713e4922a4e281cc2b46
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-12-16 13:43:38 -0800

    Fix runbench.jl

--- benchmark/runbench.jl
+++ benchmark/runbench.jl
@@ -2,7 +2,7 @@ using PkgBenchmark
 
 function ask_version()
   print("What version would you like to compare to? ")
-  chomp(strip(readline(STDIN)))
+  String(chomp(strip(readline(STDIN))))
 end
 
 results = judge("ImageQuilting", ask_version())
