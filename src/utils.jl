9439cc9e7bfbb595ea6696932017f13214dbbce4
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-05-14 22:01:27 -0300

    Move remaining utils in iqsim.jl to utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -63,6 +63,72 @@ function activation(hard::Dict, tile::CartesianIndices)
   buff
 end
 
+function preprocess_images(trainimg::AbstractArray{T,N}, soft::AbstractVector,
+                           geoconfig::NamedTuple) where {T,N}
+  padsize = geoconfig.padsize
+
+  TI = Float64.(trainimg)
+  replace!(TI, NaN => 0.)
+
+  SOFT = map(soft) do (aux, auxTI)
+    prepend = ntuple(i->0, N)
+    append  = padsize .- min.(padsize, size(aux))
+    padding = Pad(:symmetric, prepend, append)
+
+    AUX   = Float64.(padarray(aux, padding))
+    AUXTI = Float64.(auxTI)
+    replace!(AUX, NaN => 0.)
+    replace!(AUXTI, NaN => 0.)
+
+    AUX, AUXTI
+  end
+
+  TI, SOFT
+end
+
+function find_disabled(trainimg::AbstractArray{T,N}, geoconfig::NamedTuple) where {T,N}
+  TIsize   = geoconfig.TIsize
+  tilesize = geoconfig.tilesize
+  distsize = geoconfig.distsize
+
+  disabled = falses(distsize)
+  for ind in findall(isnan, trainimg)
+    start  = @. max(ind.I - tilesize + 1, 1)
+    finish = @. min(ind.I, distsize)
+    tile   = CartesianIndex(start):CartesianIndex(finish)
+    disabled[tile] .= true
+  end
+
+  disabled
+end
+
+function find_skipped(hard::Dict, geoconfig::NamedTuple)
+  ntiles   = geoconfig.ntiles
+  tilesize = geoconfig.tilesize
+  spacing  = geoconfig.spacing
+  simsize  = geoconfig.simsize
+
+  skipped = Set{Int}()
+  datainds = Vector{Int}()
+  for tileind in CartesianIndices(ntiles)
+    # tile corners are given by start and finish
+    start  = @. (tileind.I - 1)*spacing + 1
+    finish = @. start + tilesize - 1
+    tile   = CartesianIndex(start):CartesianIndex(finish)
+
+    # skip tile if either
+    #   1) tile is beyond true simulation size
+    #   2) all values in the tile are NaN
+    if any(start .> simsize) || !any(activation(hard, tile))
+      push!(skipped, cart2lin(ntiles, tileind))
+    elseif any(indicator(hard, tile))
+      push!(datainds, cart2lin(ntiles, tileind))
+    end
+  end
+
+  skipped, datainds
+end
+
 function genpath(extent::Dims{N}, kind::Symbol, datainds::AbstractVector{Int}) where {N}
   path = Vector{Int}()
 
