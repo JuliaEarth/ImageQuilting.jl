19bfce7dad512c320bce80c000836485891aac64
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-06-19 18:22:39 -0300

    Add tests for soft data

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using ImageQuilting
 using Meshes
 using GeoStatsBase
 using GeoStatsImages
+using ImageFiltering
 using Statistics
 using Plots; gr(size=(600,400))
 using ReferenceTests, ImageIO
@@ -166,6 +167,10 @@ datadir = joinpath(@__DIR__,"data")
     sdomain = CartesianGrid(100,100)
     problem = SimulationProblem(sdata, sdomain, :facies, 3)
 
+    # incomplete solver
+    solver = IQ()
+    @test_throws ErrorException solve(problem, solver)
+
     rng = MersenneTwister(2017)
     trainimg = geostatsimage("Strebelle")
     inactive = [CartesianIndex(i,j) for i in 1:30 for j in 1:30]
@@ -175,12 +180,35 @@ datadir = joinpath(@__DIR__,"data")
     @test length(solution) == 3
     @test size(domain(solution[1])) == (100,100)
 
-    # incomplete solver
-    solver = IQ()
-    @test_throws ErrorException solve(problem, solver)
+    if visualtests
+      @test_reference "data/GeoStatsAPI-1.png" plot(solution,size=(900,300))
+    end
+
+    # soft data test
+    truthimg = geostatsimage("WalkerLakeTruth")
+    trainimg = geostatsimage("WalkerLake")
+
+    # forward model (blur filter)
+    function forward(data)
+      img = asarray(data, :Z)
+      krn = KernelFactors.IIRGaussian([10,10])
+      fwd = imfilter(img, krn)
+      georef((fwd=fwd,), domain(data))
+    end
+
+    # apply forward model to both images
+    data   = forward(truthimg)
+    dataTI = forward(trainimg)
+
+    problem = SimulationProblem(domain(truthimg), :Z => Float64, 3)
+
+    rng = MersenneTwister(2017)
+    solver = IQ(:Z => (trainimg=trainimg, tilesize=(27,27), soft=(data,dataTI)), rng=rng)
+
+    solution = solve(problem, solver)
 
     if visualtests
-      @test_reference "data/GeoStatsAPI.png" plot(solution,size=(900,300))
+      @test_reference "data/GeoStatsAPI-2.png" plot(solution,size=(900,300))
     end
   end
 end
