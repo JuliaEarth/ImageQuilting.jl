6bfaac9dfeb21bc4facc100128682e90a75549db
claro_henrique <clarohenriquess@gmail.com>
2022-08-25 21:18:39 +0000

    Revert preallocate imfilter kernel

--- src/imfilter.jl
+++ src/imfilter.jl
@@ -6,31 +6,18 @@ function imfilter_cpu(img, krn)
   imfilter(img, centered(krn), Inner(), Algorithm.FFT())
 end
 
-# preallocated memory for krn optimization on GPU
-krnbuffer_gpu = nothing
-
-function alloc_krn_buffer_gpu!(sz)
-  global krnbuffer_gpu
-  
-  if isnothing(krnbuffer_gpu) || size(krnbuffer_gpu) != sz
-    krnbuffer_gpu = CuArray(zeros(sz))
-  end
-
-  krnbuffer_gpu
-end
-
 function imfilter_gpu(img, krn)
   # retrieve basic info
   N = ndims(img)
   T = eltype(img)
 
-  # copy krn data to preallocated GPU memory
-  paddedkrn = alloc_krn_buffer_gpu!(size(img))
-  copyto!(paddedkrn, CartesianIndices(krn), CuArray(krn), CartesianIndices(krn))
+  # pad kernel to common size with image
+  padsize = size(img) .- size(krn)
+  padkrn  = padarray(krn, Fill(zero(T), ntuple(i->0, N), padsize))
 
   # perform ifft(fft(img) .* conj.(fft(krn)))
   fftimg = img |> CUFFT.fft
-  fftkrn = paddedkrn |> CUFFT.fft
+  fftkrn = padkrn |> CuArray |> CUFFT.fft
   result = (fftimg .* conj.(fftkrn)) |> CUFFT.ifft
 
   # recover result
