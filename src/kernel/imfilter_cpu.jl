434e62d1ba130956b5b7f79ffe81387566ee0581
ClaroHenrique <clarohenriques@gmail.com>
2022-11-01 19:47:48 -0300

    Refact CPU imfilter

--- src/kernel/imfilter_cpu.jl
+++ src/kernel/imfilter_cpu.jl
@@ -7,5 +7,9 @@ const array_kernel(::CPUMethod, array) = array
 const view_kernel(::CPUMethod, array, I) = view(array, I)
 
 function imfilter_kernel(::CPUMethod, img, krn)
+  imfilter_cpu(img, krn)
+end
+
+function imfilter_cpu(img, krn)
   imfilter(img, centered(krn), Inner(), Algorithm.FFT())
 end
