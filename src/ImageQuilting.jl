0d50915468a5a0f37a48cb6fef9021c20a261164
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-11-06 09:37:49 -0300

    Register error hint for Makie extension (#71)

--- src/ImageQuilting.jl
+++ src/ImageQuilting.jl
@@ -27,6 +27,34 @@ include("graphcut.jl")
 include("iqsim.jl")
 include("voxelreuse.jl")
 
+function __init__()
+  # register error hint for visualization functions
+  # since this is a recurring issue for new users
+  Base.Experimental.register_error_hint(MethodError) do io, exc, argtypes, kwargs
+    if exc.f == voxelreuse || exc.f == voxelreuseplot!
+      if isnothing(Base.get_extension(ImageQuilting, :ImageQuiltingMakieExt))
+        print(
+          io,
+          """
+
+          Did you import a Makie.jl backend (e.g., GLMakie.jl, CairoMakie.jl) for visualization?
+
+          """
+        )
+        printstyled(
+          io,
+          """
+          julia> using ImageQuilting
+          julia> import GLMakie # or CairoMakie, WGLMakie, etc.
+          """,
+          color=:cyan,
+          bold=true
+        )
+      end
+    end
+  end
+end
+
 export
   # simulation
   iqsim,
